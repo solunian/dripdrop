c132d6433e36c349d138e9bbb2bb8344dafdf078
Daniel Cui <solunian@gmail.com>
2023-03-18 02:17:42 -0700

    fix link for github button

--- components/githubbutton.jsx
+++ components/githubbutton.jsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function GitHubButton() {
-    return (<a href="https://github.com/solunian/dripdrop" target="_blank">
-        <Image
-        src="/github-mark.svg"
-        width={25}
-        height={25}
-        color="white"/>
-    </a>);
+    return (<Link href="https://github.com/solunian/dripdrop">
+        <a target={'_blank'}>
+            <Image
+                src="/github-mark.svg"
+                width={25}
+                height={25}/>
+        </a>
+
+    </Link>);
 }
